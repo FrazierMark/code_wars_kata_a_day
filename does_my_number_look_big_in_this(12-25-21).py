acd18bb70bc47b7182b5335683bb55480c269ffd
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-01-28 19:29:56 -0600

    Today's Kata.

--- does_my_number_look_big_in_this(12-25-21).py
+++ does_my_number_look_big_in_this(12-25-21).py
@@ -3,22 +3,14 @@
 # of the number of digits in a given base. In this Kata,
 # we will restrict ourselves to decimal (base 10).
 
-
 def narcissistic(value):
 	str_value = str(value)
-	print(f"str_value: {str_value}")
 	base = len(str_value)
 	values = []
 	for i in str_value:
 		values.append(int(i)**base)
-	print(f"base: {base}")
-	print(f" values: {values}")
 	total = sum(values)
-	print(total)
-	if total == value:
-		print(f"true") 
-	else:
-		print(f"false")
+	return total == value
 
 
 #narcissistic(7) # True,
