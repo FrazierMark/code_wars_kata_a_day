941259a1b287a300c1f339fe5dd4736bdeca0353
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-01-29 07:03:01 -0600

    Kata for 1-29-21. Memoized Fibonacci

---
+++ memoized_fibonacci.py
@@ -0,0 +1,13 @@
+fib_dict = {}
+def fibonacci(n):
+    if n in fib_dict:
+        return fib_dict[n]
+    if n == 1 or n == 2:
+        result = 1
+    elif n > 2:
+        result = fibonacci(n - 1) + fibonacci(n - 2)
+    fib_dict[n] = result
+    return result
+
+print(fibonacci(70)) # 190392490709135)
+
