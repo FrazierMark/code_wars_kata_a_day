b4493021b47223129e5e87e7243db5abd93ee474
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-01-31 10:29:41 -0600

    kata for 1-31-21, 4 kyu

--- sum_of_intervals.py
+++ sum_of_intervals.py
@@ -1,5 +1,24 @@
-x = len(range(2:6))<<<<<
-for n in x:
-    print(n)
-    counter = len(x)
-    print(counter)
+# Write a function called sumIntervals/sum_intervals() that
+# accepts an array of intervals, and returns the sum of all
+# the interval lengths. Overlapping intervals should only be
+# counted once.
+
+
+def sum_of_intervals(intervals):
+	range_dict = {}
+	len_counter = 0
+	for array in intervals:
+		for n in range(array[0], array[-1]):
+			if n in range_dict:
+				continue
+			else:
+				range_dict[n] = n
+	print(len(range_dict))		
+
+
+
+
+# sum_of_intervals([(1, 5)])  #4)
+# sum_of_intervals([(1, 5), (6, 10)])  # 8
+# sum_of_intervals([(1, 5), (1, 5)])    #4)
+sum_of_intervals([(1, 4), (7, 10), (3, 5)]) # 7)
