662af5a87668a989ab876f9ebf61c1ce698acd8a
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-04-04 17:02:26 -0500

    kata for 04-04-21.

---
+++ reversed_sequence.py
@@ -0,0 +1,22 @@
+def reverse_seq(n):
+    x = range(1, n+1)
+    flipped = []
+    for n in x:
+    	flipped.append(n)
+    seq = flipped.reverse()
+
+    return(flipped)
+
+
+
+
+
+reverse_seq(5)
+
+
+
+
+
+# Build a function that returns an array of integers from n to 1 where n>0.
+
+# Example : n=5 --> [5,4,3,2,1]
